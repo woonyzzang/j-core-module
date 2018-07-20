500ba905828e876c8b76a4483fa90c37178ab3c1
woonyzzang <seungwoonjjang@gmail.com>
2018-07-20 14:47:09 +0900

    @doc update

---
+++ @doc/env.md
@@ -0,0 +1,45 @@
+###### j core module
+
+# {{LIB_NAME}}.env
+> 환경변수 관련 코어 확장 기능을 사용할 수 있습니다.
+
+## 확장기능
+
+- [set()](#set)
+- [get()](#get)
+
+<br>
+
+## set()
+설정값을 지정합니다.
+
+API | 설명
+--- | ---
+@param {string} | 설정명 (`.`를 구분값으로 단계를 내려가서 설정 할 수 있다.)
+@param {..any} | 설정값
+@return {..any} | 설정값
+
+```js
+{{LIB_NAME}}.Env.set('siteTitle', 'Hello World!');
+```
+
+[▲ 확장기능 목록 이동](#확장기능)
+
+<br>
+
+## get()
+설정값을 가져옵니다.
+
+API | 설명
+--- | ---
+@param {string} | 설정명 (`.`를 구분값으로 단계별로 값을 가져올 수 있다.)
+@param {...any} | 설정된 값이 없을 경우 사용할 기본값
+@return {...any} | 설정값
+
+```js
+{{LIB_NAME}}.Env.get('siteTitle');
+
+// 'Hello World!'
+```
+
+[▲ 확장기능 목록 이동](#확장기능)
--- src/lib/Addon/env.js
+++ src/lib/Addon/env.js
@@ -9,27 +9,6 @@ const addonEnv = () => {
         configs: {},
 
         /**
-         * @name get
-         * @description 설정값을 꺼내오는 함수
-         * @param {string} name 설정명. `.`를 구분값으로 단계별로 값을 가져올 수 있다.
-         * @param {*} [def] 설정된 값이 없을 경우 사용할 기본값
-         * @return {*} 설정값
-         * @example
-         * {{LIB_NAME}}.Env.get('siteTitle'); // '바이널'
-         */
-        get: function (name, def) {
-            let root = this.configs,
-                names = name.split('.'),
-                pair = root;
-
-            for (let i = 0, len = names.length; i < len; i++) {
-                if (!(pair = pair[names[i]])) { return def; }
-            }
-
-            return pair;
-        },
-
-        /**
          * @name set
          * @description 설정값을 지정하는 함수
          * @param {string} name 설정명. `.`를 구분값으로 단계를 내려가서 설정할 수 있다.
@@ -50,6 +29,27 @@ const addonEnv = () => {
             }
 
             return (pair[names[last]] = value);
+        },
+
+        /**
+         * @name get
+         * @description 설정값을 꺼내오는 함수
+         * @param {string} name 설정명. `.`를 구분값으로 단계별로 값을 가져올 수 있다.
+         * @param {*} [def] 설정된 값이 없을 경우 사용할 기본값
+         * @return {*} 설정값
+         * @example
+         * {{LIB_NAME}}.Env.get('siteTitle'); // 'Hello World!'
+         */
+        get: function (name, def) {
+            let root = this.configs,
+                names = name.split('.'),
+                pair = root;
+
+            for (let i = 0, len = names.length; i < len; i++) {
+                if (!(pair = pair[names[i]])) { return def; }
+            }
+
+            return pair;
         }
     });
 };
